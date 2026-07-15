a338088219118f8d5c9e0218af169782d45b8f4e
ub-victor <victoireushindi371@gmail.com>
2026-07-15 14:14:43 +0200

    Data access object for person

--- src/main/java/com/example/demo/dao/PersonDao.java
+++ src/main/java/com/example/demo/dao/PersonDao.java
@@ -1,5 +1,14 @@
 package com.example.demo.dao;
 
+import com.example.demo.model.Person;
+
+import java.util.UUID;
+
 public class PersonDao {
+    int insertPerson(UUID id, Person person);
+
+    default int addPerson(Person person){
+        return insertPerson(id, person);
+    }
     
 }
