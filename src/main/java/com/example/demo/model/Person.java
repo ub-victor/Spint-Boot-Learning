519dfe3ba423bc53793bbfae905cf77990e2bc85
ub-victor <victoireushindi371@gmail.com>
2026-07-15 14:07:17 +0200

    Model implementation

--- src/main/java/com/example/demo/model/Person.java
+++ src/main/java/com/example/demo/model/Person.java
@@ -11,6 +11,14 @@ public class Person {
         this.name = name;
     }
 
+    public UUID getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
     
 
     
