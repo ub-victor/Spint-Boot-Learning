20e69a93fb0ba51542cbab576ae6817c4ec80e09
ub-victor <victoireushindi371@gmail.com>
2026-07-15 19:11:04 +0200

    Service implementation

--- src/main/java/com/example/demo/service/PersonService.java
+++ src/main/java/com/example/demo/service/PersonService.java
@@ -1,9 +1,18 @@
 package com.example.demo.service;
-import com.example.demo.model.Person;
 import com.example.demo.dao.PersonDao;
+import com.example.demo.model.Person;
 
 public class PersonService {
 
-    private final ;
+    private final PersonDao personDao;
+
+    public PersonService(PersonDao personDao) {
+        this.personDao = personDao;
+    }
+
+    public int addPerson(Person person){
+        return personDao.insertPerson(person);
+    }
+
     
 }
