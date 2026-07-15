5349812e257e49e02b44cc6cc17edbc840a0e022
ub-victor <victoireushindi371@gmail.com>
2026-07-15 14:05:46 +0200

    Model implementation

---
+++ src/main/java/com/example/demo/model/Person.java
@@ -0,0 +1,17 @@
+package com.example.demo.model;
+
+import java.util.UUID;
+
+public class Person {
+    private final UUID id;
+    private final String name;
+    
+    public Person(UUID id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    
+
+    
+}
