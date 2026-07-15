7e1c8fbe8241a950667ec395043c504c31767c20
ub-victor <victoireushindi371@gmail.com>
2026-07-15 18:15:06 +0200

    Start the implementation

--- src/main/java/com/example/demo/dao/FakePersonDataAccessService.java
+++ src/main/java/com/example/demo/dao/FakePersonDataAccessService.java
@@ -1,5 +1,16 @@
 package com.example.demo.dao;
 
+import java.util.UUID;
+
+import com.example.demo.model.Person;
+
 public class FakePersonDataAccessService implements PersonDao {
+
+    @Override
+    public int insertPerson(UUID id, Person person) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+    
     
 }
